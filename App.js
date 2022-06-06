267ba4265380e75e5f41a602d6e8f1a93af245e4
Baaba <baabadampare@gmail.com>
2022-06-06 15:54:16 +0000

    add axios function

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { SafeAreaView } from "react-native";
-import { ScrollView } from "react-native";
-import Searchbar from './Components/Searchbar';
+import {
+  ScrollView,
+  TextInput,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 import tw from "twrnc";
 import WeatherImages from "./Components/WeatherImages";
 import Temperature from "./Components/Temperature";
 import WeatherWidget from "./Components/WeatherWidget";
 import WarningWidget from "./Components/WarningWidget";
+import axios from "axios"
+
 
 export default function App() {
+
+  const [town, setTown] = useState("")
+
+  
+  const getWeatherData = async () => {
+    if (town === ""){
+      const res = await axios.get(
+        "https://api.openweathermap.org/data/2.5/weather?q=accra&appid=d467205cb02234512d5a273cb405f713"
+      );
+      const data = await res.data
+      console.log(data);
+    } else {
+      try {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=d467205cb02234512d5a273cb405f713`
+        );
+        const data = await res.data;
+        console.log(data);
+      } catch (error) {
+        console.log(error)
+        console.log("Error! Invalid input")
+      }
+    }
+  }
+
+  getWeatherData()
+  
+
   return (
     <SafeAreaView style={tw`bg-[#ffff]`}>
       <ScrollView style={tw`px-6`}>
-        <Searchbar />
+        <View
+          style={tw`p-3 w-full bg-[#FDFCFC] rounded-xl items-center flex flex-row justify-between`}
+        >
+          <TextInput
+            placeholder="Search Location"
+            style={tw`text-xl`}
+            onChangeText={(town) => setTown(town)}
+          />
+          <TouchableOpacity onPress={getWeatherData}>
+            <View>
+              <FontAwesome name="search" style={tw`text-xl text-gray-300`} />
+            </View>
+          </TouchableOpacity>
+        </View>
         <WeatherImages />
         <Temperature />
-        <WeatherWidget/>
-        <WarningWidget/>
+        <WeatherWidget />
+        <WarningWidget />
       </ScrollView>
     </SafeAreaView>
   );
